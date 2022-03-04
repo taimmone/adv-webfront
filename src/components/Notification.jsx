9fbd922767da5e642962fbf5282df8e8f9643d1f
Toni Immonen <toni.immonen@tuni.fi>
2022-03-04 17:25:44 +0200

    Assignment: Implement notification component

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -4,6 +4,33 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { removeNotification } from '../redux/actionCreators/notificationsActions';
 
-const Notification = () => {};
+const Notification = () => {
+  const { notification } = useSelector(state => state);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (notification.message) {
+      const notificationTimer = setTimeout(() => {
+        dispatch(removeNotification());
+      }, 5000);
+      return () => clearTimeout(notificationTimer);
+    }
+  }, [notification]);
+
+  if (!notification.message) {
+    return <div data-testid="no-notification-component" />;
+  } else {
+    return (
+      <div
+        data-testid="notification-component"
+        style={{
+          backgroundColor: `${notification.isSuccess ? 'green' : 'red'}`,
+        }}
+      >
+        {notification.message}
+      </div>
+    );
+  }
+};
 
 export default Notification;
