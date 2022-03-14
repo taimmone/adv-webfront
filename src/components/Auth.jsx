1ea851ef6fdeaf96ff0b8452056a0125e5c97463
Toni Immonen <toni.immonen@tuni.fi>
2022-03-14 09:56:52 +0200

    Assignment: Implement Auth component

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -2,10 +2,24 @@
 
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router';
-import { Outlet } from 'react-router-dom';
+import { Outlet, Navigate, useLocation } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 
-const Auth = ({ authRoles }) => {};
+const Auth = ({ authRoles }) => {
+  const { auth } = useSelector(state => state);
+  const location = useLocation();
+
+  if (authRoles.includes(auth.role)) {
+    return (
+      <div data-testid="auth-success-component">
+        <Outlet />
+      </div>
+    );
+  } else if (auth.role === 'guest') {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  } else {
+    return <Navigate to="/" replace />;
+  }
+};
 
 export default Auth;
