9b5373b7095cae937f39be95addea24e848ab40b
Toni Immonen <toni.immonen@tuni.fi>
2022-03-25 12:59:04 +0200

    Assignment: Implement action creators for users

--- src/redux/actionCreators/usersActions.js
+++ src/redux/actionCreators/usersActions.js
@@ -2,22 +2,24 @@
 
 // USERS ACTION CREATORS
 
+import axios from 'axios';
 import {
-	GET_USER,
-	GET_USERS,
-	NEW_NOTIFICATION,
-	REMOVE_USER,
-	UPDATE_USER,
+  GET_USER,
+  GET_USERS,
+  NEW_NOTIFICATION,
+  REMOVE_USER,
+  UPDATE_USER,
 } from '../constants';
+import { createNotification } from './notificationsActions';
 
 //Use these for the notifications sent.
 const userMsg = {
-	gotUser: 'Single user received',
-	gotUsers: 'Users received',
-	updateUser: 'User updated.',
-	delete: (user) => {
-		return `${user.name} deleted successfully`;
-	},
+  gotUser: 'Single user received',
+  gotUsers: 'Users received',
+  updateUser: 'User updated.',
+  delete: user => {
+    return `${user.name} deleted successfully`;
+  },
 };
 
 /**
@@ -27,14 +29,32 @@ const userMsg = {
  * @param {String} userId - The users id that is to be fetched.
  * @returns {Function} - For the thunk to then dispatch as an object (ie the action).
  */
-export const getUser = (userId) => {};
+export const getUser = userId => dispatch =>
+  axios
+    .get(`/api/users/${userId}`)
+    .then(({ data }) => dispatch({ type: GET_USER, payload: data }))
+    .catch(error => {
+      if (error.response) {
+        const message = error.response.data.error;
+        dispatch(createNotification({ message, isSuccess: false }));
+      }
+    });
 /**
  * @description Asynchronous action creator that gets all the users from the backend (if possible) and sends that Array through thunk to the reducers.
  * If the response is not ok, it only dispatches a NEW_NOTIFICATION-type action to the frontends notification state along with the error message from db as an unsuccessfull message.
  *
  * @returns {Function} - For the thunk to then dispatch as an object (ie the action).
  */
-export const getUsers = () => {};
+export const getUsers = () => dispatch =>
+  axios
+    .get('/api/users')
+    .then(({ data }) => dispatch({ type: GET_USERS, payload: data }))
+    .catch(error => {
+      if (error.response) {
+        const message = error.response.data.error;
+        dispatch(createNotification({ message, isSuccess: false }));
+      }
+    });
 /**
  * @description Asynchronous action creator that updates the given user (if possible) and sends the user received from the backend through thunk to reducers.
  * If the response is not ok, it only dispatches a NEW_NOTIFICATION-type action to the frontends notification state along with the error message from db as an unsuccessfull message.
@@ -42,7 +62,21 @@ export const getUsers = () => {};
  * @param {object} updatedUser - contains the updated user data
  * @returns {Function} - For the thunk to then dispatch as an object (ie the action).
  */
-export const updateUser = (updatedUser) => {};
+export const updateUser = updatedUser => dispatch =>
+  axios
+    .put(`/api/users/${updatedUser.id}`, { role: updatedUser.role })
+    .then(({ data }) => {
+      dispatch({ type: UPDATE_USER, payload: data });
+      dispatch(
+        createNotification({ message: userMsg.updateUser, isSuccess: true })
+      );
+    })
+    .catch(error => {
+      if (error.response) {
+        const message = error.response.data.error;
+        dispatch(createNotification({ message, isSuccess: false }));
+      }
+    });
 /**
  * @description Removes the user (if possible) from the backend, then dispatches an action to remove it from the redux-store, as well as another action to notify the current user that the deletion was succesfull.
  * If the response is not ok, it only dispatches a NEW_NOTIFICATION-type action to the frontends notification state along with the error message from db as an unsuccessfull message.
@@ -50,4 +84,18 @@ export const updateUser = (updatedUser) => {};
  * @param {String} - The users id that is to be fetched
  * @returns {Function} - For the thunk to then dispatch as an object (ie the action).
  */
-export const removeUser = (userId) => {};
+export const removeUser = userId => dispatch =>
+  axios
+    .delete(`/api/users/${userId}`)
+    .then(({ data }) => {
+      dispatch({ type: REMOVE_USER, payload: data });
+      dispatch(
+        createNotification({ message: userMsg.delete(data), isSuccess: true })
+      );
+    })
+    .catch(error => {
+      if (error.response) {
+        const message = error.response.data.error;
+        dispatch(createNotification({ message, isSuccess: false }));
+      }
+    });
