908357a44619e34c2a91117fb1119ba8ba9cb533
Toni Immonen <toni.immonen@tuni.fi>
2022-03-25 11:55:00 +0200

    Assignment: Implement action creators for orders

--- src/redux/actionCreators/ordersActions.js
+++ src/redux/actionCreators/ordersActions.js
@@ -2,16 +2,19 @@
 
 // ORDER ACTION CREATORS
 
+import axios from 'axios';
 import {
-	NEW_NOTIFICATION,
-	GET_ORDERS,
-	ADD_ORDER,
-	GET_ORDER,
+  NEW_NOTIFICATION,
+  GET_ORDERS,
+  ADD_ORDER,
+  GET_ORDER,
+  EMPTY_CART,
 } from '../constants';
 import { emptyCart } from './cartActions';
+import { createNotification } from './notificationsActions';
 
 const orderMsg = {
-	newOrder: 'New order made.',
+  newOrder: 'New order made.',
 };
 /**
  * @description Action creator for getting a single order. Dispatches action with type GET_ORDER and payload of the fetched order if succesfull.
@@ -19,14 +22,34 @@ const orderMsg = {
  * @param {String} orderId -  The id of the order to get
  * @return {Function} - Thunk -> action
  */
-export const getOrder = (orderId) => {};
+export const getOrder = orderId => dispatch =>
+  axios
+    .get(`/api/orders/${orderId}`)
+    .then(({ data }) => dispatch({ type: GET_ORDER, payload: data }))
+    .catch(error => {
+      if (error.response) {
+        const message = error.response.data.error;
+        dispatch(createNotification({ message, isSuccess: false }));
+      }
+    });
 
 /**
  * @description Action creator for getting all orders. Dispatches action with type GET_ORDERS and payload of the fetched orders if succesfull.
  * If the response is not ok, it only dispatches a NEW_NOTIFICATION-type action to the frontends notification state along with the error message from db as an unsuccessfull message.
  * @return {Function} - Thunk -> action
  */
-export const getOrders = () => {};
+export const getOrders = () => dispatch =>
+  axios
+    .get('/api/orders')
+    .then(({ data }) => {
+      dispatch({ type: GET_ORDERS, payload: data });
+    })
+    .catch(error => {
+      if (error.response) {
+        const message = error.response.data.error;
+        dispatch(createNotification({ message, isSuccess: false }));
+      }
+    });
 
 /**
  * @description Action creator for adding a new order. Dispatches actions:
@@ -38,4 +61,19 @@ export const getOrders = () => {};
  * @param {Object} newOrder -  The new order to post
  * @return {Function} - Thunk -> action
  */
-export const addOrder = (newOrder) => {};
+export const addOrder = newOrder => dispatch =>
+  axios
+    .post('/api/orders', newOrder)
+    .then(({ data }) => {
+      dispatch(emptyCart());
+      dispatch({ type: ADD_ORDER, payload: data });
+      dispatch(
+        createNotification({ message: orderMsg.newOrder, isSuccess: true })
+      );
+    })
+    .catch(error => {
+      if (error.response) {
+        const message = error.response.data.error;
+        dispatch(createNotification({ message, isSuccess: false }));
+      }
+    });
