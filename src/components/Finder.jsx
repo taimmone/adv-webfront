900842ef2599a24517ed7bfb76409e50e1cbde2e
Toni Immonen <toni.immonen@tuni.fi>
2022-03-28 10:17:10 +0300

    Assignment: Implement Finder component

--- src/components/Finder.jsx
+++ src/components/Finder.jsx
@@ -5,6 +5,31 @@ import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Outlet, useParams } from 'react-router-dom';
 
-const Finder = ({ type, findHandler }) => {};
+const Finder = ({ type, findHandler }) => {
+  const state = useSelector(state => state[`${type}s`]);
+  const itemId = useParams()[`${type}Id`];
+  const dispatch = useDispatch();
+
+  const findItem = () => state?.find(item => item.id === itemId);
+
+  // console.log(type, itemId, findItem());
+
+  useEffect(() => {
+    if (!findItem()) {
+      dispatch(findHandler(itemId));
+    }
+  }, [itemId]);
+
+  if (!findItem())
+    return (
+      <div data-testid={`no-${type}-found-component`}>{type} not found.</div>
+    );
+  else
+    return (
+      <div data-testid={`${type}-found-component`}>
+        <Outlet context={findItem()} />
+      </div>
+    );
+};
 
 export default Finder;
