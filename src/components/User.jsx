e84d5cbbb2db7315530d6b9202fe4a649be4584d
Toni Immonen <toni.immonen@tuni.fi>
2022-03-30 16:43:12 +0300

    Assignment: Implement User component

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -5,6 +5,59 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { removeUser } from '../redux/actionCreators/usersActions';
 
-const User = ({ providedUser }) => {};
+const User = ({ providedUser }) => {
+  const { auth, users } = useSelector(state => state);
+  const { userId } = useParams();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { name, email, role } =
+    providedUser ?? users.find(p => p.id === userId);
+
+  const getId = () => providedUser?.id ?? userId;
+
+  const handleModify = () => {
+    navigate(userId ? 'modify' : `${getId()}/modify`);
+  };
+
+  const handleDelete = () => {
+    dispatch(removeUser(userId));
+  };
+
+  return (
+    <div data-testid="user-component">
+      <h2 data-testid="name-heading">{name}</h2>
+      {!userId && (
+        <Link to={providedUser.id} data-testid="inspect-link">
+          Inspect
+        </Link>
+      )}
+      <p data-testid="email-element">Email: {email}</p>
+      <p data-testid="role-element">Role: {role}</p>
+      {auth?.id !== getId() && (
+        <div>
+          <button
+            onClick={handleModify}
+            data-testid={`modify-button-${getId()}`}
+          >
+            Modify
+          </button>
+          <button
+            onClick={handleDelete}
+            data-testid={`delete-button-${getId()}`}
+          >
+            Delete
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+const UserButton = ({ text, onClick, testId }) => (
+  <button type="button" onClick={onClick} data-testid={testId}>
+    {text}
+  </button>
+);
 
 export default User;
