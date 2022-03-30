1dc696763daf704ba7f0117b04d1b0442ddab160
Toni Immonen <toni.immonen@tuni.fi>
2022-03-30 18:24:41 +0300

    Assignment: Implement Users component

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -5,6 +5,25 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getUsers } from '../redux/actionCreators/usersActions';
 import User from './User';
 
-const Users = () => {};
+const Users = () => {
+  const { users } = useSelector(state => state);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!users.length) dispatch(getUsers());
+  }, []);
+
+  return (
+    <div data-testid="users-component">
+      <ul data-testid="users-container">
+        {users?.map(user => (
+          <li key={user.id}>
+            <User providedUser={user} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Users;
