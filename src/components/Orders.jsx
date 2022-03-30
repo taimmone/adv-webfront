00a62e3019ab39c26fc566699630ca9ee374be7d
Toni Immonen <toni.immonen@tuni.fi>
2022-03-30 20:11:48 +0300

    Assignment: Implement Orders component

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -5,6 +5,30 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getOrders } from '../redux/actionCreators/ordersActions';
 import Order from './Order';
 
-const Orders = () => {};
+const Orders = () => {
+  const { orders } = useSelector(state => state);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!orders.length) dispatch(getOrders());
+  }, []);
+
+  console.table(orders);
+  console.log(orders.length);
+
+  if (!orders.length)
+    return <div data-testid="no-order-component">No orders</div>;
+  return (
+    <div data-testid="orders-component">
+      <ul data-testid="orders-container">
+        {orders.map(order => (
+          <li key={order.id}>
+            <Order providedOrder={order} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Orders;
