3e3d211a795492549ef34bd17b3260200bcfdef9
Toni Immonen <toni.immonen@tuni.fi>
2022-03-30 17:05:20 +0300

    Assignment: Implement UserModifier component

--- src/components/UserModifier.jsx
+++ src/components/UserModifier.jsx
@@ -5,6 +5,50 @@ import { useDispatch, useSelector } from 'react-redux';
 import { updateUser } from '../redux/actionCreators/usersActions';
 import { useNavigate, useParams } from 'react-router-dom';
 
-const UserModifier = () => {};
+const UserModifier = () => {
+  const { userId } = useParams();
+  const { users } = useSelector(state => state);
+
+  const oldUser = users.find(u => u.id === userId);
+  const [updatedUser, setUser] = useState({
+    ...oldUser,
+  });
+
+  const [selectedRole, setRole] = useState(oldUser.role);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    dispatch(updateUser(updatedUser));
+    navigate('/users');
+  };
+
+  return (
+    <div>
+      <h1>Modify user</h1>
+      <form onSubmit={handleSubmit} data-testid="user-modifier-component">
+        <h2 data-testid="name-heading">{oldUser.name}</h2>
+        <select
+          name="role-select"
+          value={selectedRole}
+          onChange={e => setRole(e.target.value)}
+          data-testid="role-select"
+        >
+          <option value="customer">customer</option>
+          <option value="admin">admin</option>
+        </select>
+        <button
+          type="submit"
+          data-testid="update-button"
+          disabled={selectedRole === oldUser.role}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default UserModifier;
