08d9b17523b18d2d4b3e18a02d1a21d26383cae4
Toni Immonen <toni.immonen@tuni.fi>
2022-03-31 06:50:16 +0300

    Assignment: Implement CartItem component

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -3,11 +3,46 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import {
-	decrementCartItem,
-	incrementCartItem,
-	removeCartItem,
+  decrementCartItem,
+  incrementCartItem,
+  removeCartItem,
 } from '../redux/actionCreators/cartActions';
 
-const CartItem = ({ item }) => {};
+const CartItem = ({ item }) => {
+  const { product, quantity } = item;
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => {
+    dispatch(incrementCartItem(product.id));
+  };
+  const handleDecrement = () => {
+    quantity - 1
+      ? dispatch(decrementCartItem(product.id))
+      : dispatch(removeCartItem(product));
+  };
+
+  return (
+    <div data-testid="cart-item-component">
+      <p data-testid="item-name">{product.name}</p>
+      <p data-testid="item-price">{product.price}</p>
+      <p data-testid="item-amount">{quantity}</p>
+      <div>
+        <button
+          onClick={handleIncrement}
+          data-testid={`plus-btn-${product.id}`}
+        >
+          +
+        </button>
+        <button
+          onClick={handleDecrement}
+          data-testid={`minus-btn-${product.id}`}
+        >
+          -
+        </button>
+      </div>
+      product
+    </div>
+  );
+};
 
 export default CartItem;
